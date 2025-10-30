b693e2725201914062d3835aac1cb5c5d834db10
dylanneedhelp <dangvanbo2004@gmail.com>
2025-10-30 12:24:56 +0700

    add role controller

---
+++ GustoSystemProject/Controllers/RoleController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using Repository.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GustoSystemProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleService _roleService;
+        public RoleController(RoleService roleService)
+        {
+            _roleService = roleService;
+        }
+        // GET: api/<RoleController>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<List<Role>>  Get()
+        {
+            return await _roleService.GetAllRoles();
+        }
+
+        // GET api/<RoleController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RoleController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RoleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RoleController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
